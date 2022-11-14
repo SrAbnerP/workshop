bd0328c8bde5e950ddce811e81f280bf46a2237f
Abner <abner.sol.pereira@gmail.com>
2022-11-14 20:28:15 -0300

    Workshop
    Creating workshop in Spring Boot

---
+++ workshop/src/test/java/com/projeto/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
